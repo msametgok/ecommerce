a87ef02d772389f5edf95140efe30a9e12284163
Mehmet Samet Gok <msametgok@hotmail.com>
2026-04-29 21:24:49 +0300

    use beforeEach to remove duplication

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi } from 'vitest';
+import { it, expect, describe, vi, beforeEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import axios from 'axios';
@@ -7,19 +7,26 @@ import { Product } from './Product';
 vi.mock('axios');
 
 describe('Product component', () => {
-    const product = {
-            id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-            image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-            name: "Adults Plain Cotton T-Shirt - 2 Pack",
-            rating: {
-                stars: 4.5,
-                count: 56
-            },
-            priceCents: 799,
-            keywords: ["tshirts", "apparel", "mens"]
-        }
 
-        const loadCart = vi.fn();
+        let product; 
+        let loadCart;
+
+        beforeEach(() => {
+            product = {
+                id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+                image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+                name: "Adults Plain Cotton T-Shirt - 2 Pack",
+                rating: {
+                    stars: 4.5,
+                    count: 56
+                },
+                priceCents: 799,
+                keywords: ["tshirts", "apparel", "mens"]
+            }
+
+            loadCart = vi.fn();
+
+        })
     it('displays the product details correctly', () => {
 
         render(<Product product={product} loadCart={loadCart} />);
